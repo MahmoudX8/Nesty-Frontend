0652ee0a2e4e2ffe6a95d3265681cbf0054bc729
mahmoudalaa <135064108+MahmoudX8@users.noreply.github.com>
2026-08-04 01:46:43 +0300

    Update RootLayout.jsx

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -1,13 +1,10 @@
 import { Outlet } from 'react-router-dom'
 import { AuthProvider } from '../AuthProvider'
 import { CartProvider } from '../CartContext'
-import { OrderProvider } from '../OrderContext'
 export const RootLayout = () => {
   return( 
     <AuthProvider>
       <CartProvider>
-        {/* <OrderProvider>
-        </OrderProvider> */}
           <Outlet />
       </CartProvider>
     </AuthProvider>
