8bc800fdbc3be534ae19fec1ab5ebc45d6c8cfaa
mahmoudalaa <135064108+MahmoudX8@users.noreply.github.com>
2026-08-13 20:19:58 +0300

    Update NotFound.jsx

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import '../styles/pages/notfound.css';
+import { useNavigate } from 'react-router-dom';
 import { IoMdArrowRoundBack } from "react-icons/io";
-
 export const NotFound = () => {
+  const navigate = useNavigate();
   return (
     <>
         <div className="notfound">
@@ -10,9 +11,9 @@ export const NotFound = () => {
               <h1>404 Page</h1>
               <h1>Not Found </h1>
           </div>
-        </div>
-        <div className="btn">
-            <button onClick={()=>{navigate('/'); return;}}>Go Back To Home <IoMdArrowRoundBack className='backarrow'/></button>
+            <div className="btn">
+              <button onClick={()=>{navigate('/'); return;}}>Go Back To Home <IoMdArrowRoundBack className='backarrow'/></button>
+            </div>
         </div>
     </>
   )
