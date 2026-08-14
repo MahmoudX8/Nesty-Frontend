3a583cba1d92d94285d251c6062acdae453ac09a
mahmoudalaa <135064108+MahmoudX8@users.noreply.github.com>
2026-08-15 02:40:57 +0300

    Update Signup.jsx

--- src/pages/auth/Signup.jsx
+++ src/pages/auth/Signup.jsx
@@ -11,11 +11,9 @@ export const Signup = () => {
     const [lname,setlname] = useState("");
     const [email,setemail] = useState("");
     const [pass,setpass] = useState("");
-    // const [cpass,setcpass] = useState("");
     const [err,seterr] = useState("");
     const [success,setsuccess] = useState("");
     const [token,settoken] = useState("");
-    // const [role,setrole] = useState("");
     const [showPass, setShowPass] = useState(false);
     const navigate = useNavigate();
     const {login} = useAuth();
@@ -24,7 +22,6 @@ export const Signup = () => {
         setloading(true);
         seterr("");
         try {
-            // 1. Get IP
             const ipResponse = await axios.get('https://api.ipify.org?format=json' ,{
                 withCredentials: false
             });
@@ -41,8 +38,6 @@ export const Signup = () => {
             if(data.success){
                 localStorage.setItem("email", data.email);
                 localStorage.setItem("name", fname);
-                // settoken(data.accessToken);
-                // signup(data.accessToken);
                 setsuccess(data.message);
                 setTimeout(() => {
                     navigate('/verify-otp/signup');
@@ -66,8 +61,8 @@ export const Signup = () => {
     <div className="signuppage">
         <form onSubmit={handlesubmit}>
             <h1 className='titleofform'>Signup</h1>
-            {err && <p style={{color:"red"}}>{err}</p>}
-            {success && <p style={{color: 'green'}} >{success}</p>}
+            {err && <div className='errdiv'><p style={{color:"rgb(255, 0, 0)"}}>{err}</p></div>}
+            {success && <div className='successdiv'><p style={{color: 'green'}} >{success}</p></div>}
             <input type="text" required placeholder='First Name' value={fname} disabled={loading} onChange={(e)=>{setfname(e.target.value)}} />
             <input type="text" required placeholder='Last Name' value={lname} disabled={loading} onChange={(e)=>{setlname(e.target.value)}} />
             <input type="email" required placeholder='Email' value={email} disabled={loading} onChange={(e)=>{setemail(e.target.value)}} />
@@ -75,7 +70,7 @@ export const Signup = () => {
                 <input type={showPass?"text":"password"} className='password' required placeholder='Password' value={pass} disabled={loading} onChange={(e)=>{setpass(e.target.value)}} />
                 <span><FaEyeSlash className='eye' style={showPass?{color:'#50624e'}:{color:'gray'}} onClick={(e)=>{e.stopPropagation(); setShowPass(!showPass)}}/></span>
             </div>
-            <button type="submit">{loading ? "..." : "submit"}</button>
+            <button type="submit" disabled={loading}>{loading ? "..." : "submit"}</button>
             <br />
             <p className='question' onClick={()=>{navigate('/login')}}>already have account?</p>
             <p className='terms' style={{color:"grey"}}>By joining, you agree to the Nesty Terms of Service and to occasionally receive emails from us. Please read our Privacy Policy to learn how we use your personal data.</p>
